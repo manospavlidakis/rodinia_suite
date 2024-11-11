75c07391b16a88c50337657d0889a90dcfa8f96d
manospavlidakis <manospavlidakis@gmail.com>
2024-11-11 07:18:51 +0000

    fix: update makefiles to work with scalse

--- cuda/buildall.sh
+++ cuda/buildall.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
+#CUDA_DIR=/opt/cuda
+echo "Hereeeeeeeeeeeeeeeee!!!"
+CUDA_DIR=$1
+CUDA_LIB_DIR=$(CUDA_DIR)/lib64
+SM_VERSION=$2
+numeric_part="${SM_VERSION#*_}"
+echo "$numeric_part"
+
+GENCODE_FLAGS="-gencode arch=compute_${SM_VERSION},code=sm_${SM_VERSION}"
+CXXFLAGS="-std=c++11 -m64 -O3"
+
 for mf in `find -name 'Makefile'`; do                                                               
     cd `dirname $mf`                                                                                
     make clean                                                                                      
-    make -j                                                                     
-    cd -                                                                                            
+    make -j CUDA_DIR="$CUDA_DIR" GENCODE_FLAGS="$GENCODE_FLAGS" CXXFLAGS="$CXXFLAGS" CUDA_LIB_DIR="$CUDA_LIB_DIR"
+    cd -                   
 done    
