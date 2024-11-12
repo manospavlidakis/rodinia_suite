700305030b3ec70b5a6dc18439ba2eb3339977da
manospavlidakis <manospavlidakis@gmail.com>
2024-11-12 12:24:57 +0000

    fix: rm unused scripts from hip

--- hip/compile.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-for mf in `find -name 'Makefile'`; do                                                               
-    cd `dirname $mf`                                                                                
-    make clean                                                                                      
-    make -j                                                                     
-    cd -                                                                                            
-done  
