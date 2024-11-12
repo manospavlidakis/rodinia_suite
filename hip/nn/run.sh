b8083cf042cb513394aae656c7e25be2afb87ed7
manospavlidakis <manospavlidakis@gmail.com>
2024-11-12 10:11:46 +0000

    fix: paths in files were wrong

--- hip/nn/run.sh
+++ hip/nn/run.sh
@@ -3,5 +3,5 @@ path=$1
 file=$2
 for ((iter=1; iter<=5;iter++))
 do
-./nn ../../data/nn/inputGen/list2048k.txt -r 5 -lat 30 -lng 90 -q &> 2048_${iter}_${file}
+./nn list2048k.txt -r 5 -lat 30 -lng 90 -q &> 2048_${iter}_${file}
 done
