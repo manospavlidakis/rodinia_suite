e92e2ef197a1289fc3e2e3741d30cf0675a979de
manospavlidakis <manospavlidakis@gmail.com>
2024-11-13 12:00:45 +0000

    update build all to contain debug flag

--- cuda/buildall.sh
+++ cuda/buildall.sh
@@ -19,6 +19,10 @@ echo "SM version : ${SM_VERSION}"
 GENCODE_FLAGS="-gencode arch=compute_${SM_VERSION},code=sm_${SM_VERSION}"
 CXXFLAGS="-std=c++11 -m64 -O3"
 
+#if [ "$d" = "debug" ]; then
+CXXFLAGS+=" -DOUTPUT"
+#fi
+
 for mf in `find -name 'Makefile'`; do                                                               
     cd `dirname $mf`                                                                                
     make clean                                                                                      
