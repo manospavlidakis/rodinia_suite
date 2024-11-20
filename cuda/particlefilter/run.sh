84fb9c242e053d6878ea4cc0e33673cff9f84d15
manospavlidakis <manospavlidakis@gmail.com>
2024-11-20 09:33:48 +0000

    fix: update run scripts

--- cuda/particlefilter/run.sh
+++ cuda/particlefilter/run.sh
@@ -4,7 +4,7 @@ file=$2
 benchmark_name="${file%.csv}"
 for ((iter=1; iter<=5;iter++))
 do
-./${benchmark_name} -x 128 -y 128 -z 100 -np 1000 &> 128_100_1000_${iter}_${file}
+./${benchmark_name}_float -x 128 -y 128 -z 100 -np 1000 &> 128_100_1000_${iter}_${file}
 done
 ../find_avg_per_app.py  ${benchmark_name}
 # ./particlefilter_float -x 256 -y 256 -z 10 -np 10 &> 256_10_10_${file}
