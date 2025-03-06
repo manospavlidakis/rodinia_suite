251b7068d39a05e843c13fdfe8b2f06c3d8b9d0e
manospavlidakis <manospavlidakis@gmail.com>
2025-03-06 07:35:38 +0000

    updated compile and clean scripts

---
+++ hip/cleanall.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+SPECTRAL=$3
+
+if [ "$SPECTRAL" = true ]; then
+    CUDA_DIR=$1
+    SM_VERSION=$2
+    # Code that runs if FLAG is true
+    echo "Run with Spectral"
+else
+    CUDA_DIR=/opt/cuda-11.7
+    SM_VERSION="86"
+    # Code that runs if FLAG is not true
+    echo "FLAG is not defined"
+fi
+echo "CUDA dir: ${CUDA_DIR}"
+CUDA_LIB_DIR=$CUDA_DIR/lib64
+echo "SM version : ${SM_VERSION}"
+
+GENCODE_FLAGS="-gencode arch=compute_${SM_VERSION},code=sm_${SM_VERSION}"
+CXXFLAGS="-std=c++11 -m64 -O3"
+
+#if [ "$d" = "debug" ]; then
+#CXXFLAGS+=" -DOUTPUT"
+#fi
+
+for mf in `find -name 'Makefile'`; do                                                               
+    cd `dirname $mf`                                                                                
+    make clean                                                                                      
+    cd -                   
+done    
---
+++ hip/nn/compile.sh
@@ -0,0 +1,1 @@
+hipcc -O2 -fstrict-aliasing nn_hip.cpp -o nn
