66c7256e91a0f6c37a1724df93d414c627b520dd
manospavlidakis <manospavlidakis@gmail.com>
2025-03-10 10:32:31 +0000

    update run script

--- hipify_cuda/nn/run.sh
+++ hipify_cuda/nn/run.sh
@@ -4,6 +4,6 @@ file=$2
 benchmark_name="${file%.csv}"
 for ((iter=1; iter<=30;iter++))
 do
-./${benchmark_name} list2048k.txt -r 5 -lat 30 -lng 90 -q &> 2048_${iter}_${file}
+./${benchmark_name} ../../hip/nn/list2048k.txt -r 5 -lat 30 -lng 90 -q &> 2048_${iter}_${file}
 done
 ../find_avg_per_app.py  ${benchmark_name}
