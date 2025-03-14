8fc5906f516322e2316d2befe881f197b94e25c5
manospavlidakis <manospavlidakis@gmail.com>
2025-03-14 19:53:32 +0000

    implement dwt2d in hip

---
+++ hipify_cuda/dwt2d/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+path=$1
+file=$2
+benchmark_name="${file%.csv}"
+#echo "Name: ${benchmark_name}"
+#echo "filename: "${file}
+#echo "path: "${path}
+for ((iter=1; iter<=30;iter++))
+do
+./${benchmark_name} rgb.bmp -d 1024x1024 -f -5 -l 3 &>53_${iter}_${file}
+done
+../find_avg_per_app.py  ${benchmark_name}
